f6ae365636b90ed5f04976ed039e790cfbd51b80
Tobias Boceck <boceckts@gmail.com>
2018-11-09 15:02:08 +0100

    Removed name field from detail view

--- src/components/AntiPatterns.vue
+++ src/components/AntiPatterns.vue
@@ -28,14 +28,6 @@
                                     <v-list>
                                         <v-list-tile>
                                             <v-list-tile-content>
-                                                <v-list-tile-title>Name</v-list-tile-title>
-                                                <v-list-tile-sub-title>{{ antiPattern.name }}
-                                                </v-list-tile-sub-title>
-                                            </v-list-tile-content>
-                                        </v-list-tile>
-
-                                        <v-list-tile>
-                                            <v-list-tile-content>
                                                 <v-list-tile-title>Aliases</v-list-tile-title>
                                                 <v-list-tile-sub-title>{{ antiPattern.aliases }}
                                                 </v-list-tile-sub-title>
