27add081aaf1fca1e661411337ed4c4f9e05eacb
Matthias Popp <matthias.popp95@gmx.de>
2018-11-11 18:36:44 +0100

    add toasted

--- src/components/AntiPatternActionsComponent.vue
+++ src/components/AntiPatternActionsComponent.vue
@@ -1,19 +1,25 @@
 <template>
     <span>
         <v-tooltip top>
-            <v-btn slot="activator" icon v-clipboard:copy="antiPatternAsJson">
+            <v-btn slot="activator" icon v-clipboard="antiPatternAsJson"
+                   @success="onCopySuccess('JSON file')"
+                   @error="onCopyError('JSON file')">
                 <v-icon>content_copy</v-icon>
             </v-btn>
             <span>Copy the json file</span>
         </v-tooltip>
         <v-tooltip top>
-            <v-btn slot="activator" icon v-clipboard:copy="antiPattern.sources[0]">
+            <v-btn slot="activator" icon v-clipboard="antiPattern.sources[0]"
+                   @success="onCopySuccess('BibTex')"
+                   @error="onCopyError('BibTex')">
                 <v-icon>format_quote</v-icon>
             </v-btn>
             <span>Copy the BibTex source</span>
         </v-tooltip>
         <v-tooltip top>
-            <v-btn slot="activator" icon v-clipboard:copy="shareLink">
+            <v-btn slot="activator" icon v-clipboard="shareLink"
+                   @success="onCopySuccess('Link')"
+                   @error="onCopyError('link')">
                 <v-icon>share</v-icon>
             </v-btn>
             <span>Copy the link to this antipattern to the clipboard</span>
@@ -28,19 +34,28 @@
     @Component
     export default class AntiPatternActionsComponent extends Vue {
         @Prop(Object) public antiPattern!: AntiPattern;
-        public shareLink: string = '';
-        public antiPatternAsJson = '';
 
-        public created() {
-            let antiPatternName: string = "default";
+        public get shareLink(): string {
+            let antiPatternName: string = '';
             if (this.antiPattern.name) {
                 antiPatternName = this.antiPattern.name;
             }
-            this.antiPatternAsJson = JSON.stringify(this.antiPattern);
-            this.shareLink = window.location.origin + this.$router.resolve({
+            return window.location.origin + this.$router.resolve({
                 name: 'homeWithTerm',
                 params: {antipattern: antiPatternName},
             }).href;
+        }
+
+        public get antiPatternAsJson(): string {
+            return JSON.stringify(this.antiPattern, null, 2);
+        }
+
+        public onCopySuccess(type?: string): void {
+            this.$toasted.info(type + ' was copied successfully');
+        }
+
+        public onCopyError(type?: string): void {
+            this.$toasted.error('Error copying ' + type);
         }
     }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import Vue from 'vue';
 import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
-import VueClipboard from 'vue-clipboard2';
+import VueClipboards from 'vue-clipboards';
+import Toasted from 'vue-toasted';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.config.productionTip = false;
-Vue.use(VueClipboard);
+Vue.use(VueClipboards);
+
+// @ts-ignore
+Vue.use(Toasted, {
+    theme: "primary",
+    position: "bottom-right",
+    duration : 5000,
+});
 
 new Vue({
   router,
