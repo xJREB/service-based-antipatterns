0476b24809c11a4e9037cedc298cef83dc1e8d29
st117848@stud.uni-stuttgart.de <st117848@stud.uni-stuttgart.de>
2018-12-08 15:15:11 +0100

    Refactor of the plugins

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import './plugins/vuetify';
+import './plugins/clipboard';
+import './plugins/toasted';
 import App from './App.vue';
 import router from './router';
-import VueClipboard from 'vue-clipboard2';
-import Toasted from 'vue-toasted';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import {library} from "@fortawesome/fontawesome-svg-core";
@@ -11,21 +11,9 @@ import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 import {faGithub} from "@fortawesome/free-brands-svg-icons";
 
 Vue.config.productionTip = false;
-VueClipboard.config.autoSetContainer = true;
-Vue.use(VueClipboard);
 
 library.add(faGithub);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
-
-// @ts-ignore
-Vue.use(Toasted, {
-    theme: "primary",
-    position: "bottom-right",
-    duration: 5000,
-    iconPack: "material",
-    icon: 'info_outline',
-    className: "application",
-});
 
 new Vue({
     router,
---
+++ src/plugins/toasted.ts
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Toasted from 'vue-toasted';;
+
+// @ts-ignore
+Vue.use(Toasted, {
+    theme: "primary",
+    position: "bottom-right",
+    duration: 5000,
+    iconPack: "material",
+    icon: 'info_outline',
+    className: "application",
+});
