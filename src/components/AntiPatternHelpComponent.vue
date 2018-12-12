78277d58b2655013fa32414629f3f626bb27e98d
Tobias Boceck <boceckts@gmail.com>
2018-12-12 09:58:54 +0100

    Added markdown viewer to display markdown help files (#47)

---
+++ src/components/AntiPatternHelpComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-card>
+        <v-toolbar class="secondary" card dark extended>
+            <font-awesome-icon size="2x" :icon="['far', 'question-circle']"></font-awesome-icon>
+            <v-toolbar-title>About Service-Based Antipatterns</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-tooltip top>
+                <v-btn slot="activator" icon
+                       @click="closeDialog">
+                    <v-icon>close</v-icon>
+                </v-btn>
+                <span>Close the dialog</span>
+            </v-tooltip>
+            <v-tabs color="transparent" slot="extension" fixed-tabs v-model="tab">
+                <v-tab v-for="file in files" :key="file.name">
+                    {{file.name}}
+                </v-tab>
+            </v-tabs>
+        </v-toolbar>
+
+        <v-card-text>
+            <v-tabs-items v-model="tab">
+                <v-tab-item v-for="file in files" :key="file.content">
+                    <v-card flat>
+                        <v-card-text>
+                            <vue-markdown>{{file.content}}</vue-markdown>
+                        </v-card-text>
+                    </v-card>
+                </v-tab-item>
+            </v-tabs-items>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script lang="ts">
+    import {Component, Emit, Prop, Vue} from 'vue-property-decorator';
+    import {File} from "@/common/file";
+
+    @Component
+    export default class AntiPatternDetailComponent extends Vue {
+        @Prop(Array) public files!: File[];
+        private tab: number = 0;
+
+        @Emit('input')
+        public closeDialog() {
+            return false;
+        }
+    }
+</script>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import './plugins/vuetify';
 import './plugins/clipboard';
 import './plugins/toasted';
+import './plugins/markdown';
 import App from './App.vue';
 import router from './router';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
@@ -9,10 +10,11 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import {faQuestionCircle} from "@fortawesome/free-regular-svg-icons";
 
 Vue.config.productionTip = false;
 
-library.add(faGithub);
+library.add(faQuestionCircle, faGithub);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 new Vue({
