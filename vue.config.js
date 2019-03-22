0ab271ed7fd681c4e5c739cb7bc5b8466ee11228
Justus Bogner <justus.bogner@dxc.com>
2019-03-22 10:58:38 +0100

    Fixed production build

--- vue.config.js
+++ vue.config.js
@@ -1,43 +1,43 @@
 var MergeJsonWebpackPlugin = require("merge-jsons-webpack-plugin");
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+var CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    devServer: {
-        host: '0.0.0.0',
-        port: 8081
-    },
-    configureWebpack: {
-        plugins: [
-            new MergeJsonWebpackPlugin({
-                "debug": false,
-                "output": {
-                    "groupBy": [
-                        {
-                            "pattern": "./antipatterns/*.json",
-                            "fileName": "./assets/result.json"
-                        }
-                    ]
-                },
-                "globOptions": {
-                    "nosort": false,
-                    "prefixFileName": true,
-                }
-            }),
-            new CopyWebpackPlugin([
-                {
-                    from: './*.md',
-                    to: './assets/[name].[ext]'
-                },
-                {
-                    from: './slr/*.md',
-                    to: './assets/[name].[ext]'
-                },
-                {
-                    from: './assets/*.json',
-                    to: './assets/[name].[ext]'
-                }
-            ])
-        ]
-    },
-    publicPath: process.env.NODE_ENV === 'production' ? '<service-based-antipatterns>' : '/service-based-antipatterns/'
+  devServer: {
+    host: "0.0.0.0",
+    port: 8081
+  },
+  configureWebpack: {
+    plugins: [
+      new MergeJsonWebpackPlugin({
+        debug: false,
+        output: {
+          groupBy: [
+            {
+              pattern: "./antipatterns/*.json",
+              fileName: "./assets/result.json"
+            }
+          ]
+        },
+        globOptions: {
+          nosort: false,
+          prefixFileName: true
+        }
+      }),
+      new CopyWebpackPlugin([
+        {
+          from: "./*.md",
+          to: "./assets/[name].[ext]"
+        },
+        {
+          from: "./slr/*.md",
+          to: "./assets/[name].[ext]"
+        },
+        {
+          from: "./assets/*.json",
+          to: "./assets/[name].[ext]"
+        }
+      ])
+    ]
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "/service-based-antipatterns/" : "."
 };
